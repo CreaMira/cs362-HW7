2c65ab4cff77567ed3576cb92d8993968ddc1f01
Nuocheng Shen <shennu@oregonstate.edu>
2021-05-25 20:22:36 -0700

    FizzBuzz, Wrote second test
    Test if the multiples of five will print “Buzz.” And change the name of the first test to make the test more clear

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -1,9 +1,13 @@
 import unittest
 import fizzBuzz
 
-class testCaseAdd(unittest.TestCase):
+class testFizz(unittest.TestCase):
     def test_fizz(self):
         self.assertEqual(fizzBuzz.fizzBuzz(3), "Fizz")
+
+class testBuzz(unittest.TestCase):
+    def test_buzz(self):
+        self.assertEqual(fizzBuzz.fizzBuzz(5), "Buzz")
 
 if __name__ == '__main__':
     unittest.main()
