2098c593c287fa5ec351cd399de0b7efcc3884c9
Nuocheng Shen <shennu@oregonstate.edu>
2021-05-25 20:57:09 -0700

    leapYear, Wrote second test
    Test years are not evenly divisible by 4 will return False (is not leap year)

--- test_leapYear.py
+++ test_leapYear.py
@@ -5,5 +5,9 @@ class testBy4(unittest.TestCase):
     def test_one(self):
         self.assertEqual(leapYear.is_leapYear(8), True )
 
+class testNBy4(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(leapYear.is_leapYear(9), False )
+
 if __name__ == '__main__':
     unittest.main()
