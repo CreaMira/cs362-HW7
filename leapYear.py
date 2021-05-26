28c78d5b3877fca781a9e4c86e6481fbc97e78d9
Nuocheng Shen <shennu@oregonstate.edu>
2021-05-25 21:02:49 -0700

    Implemented divisible by 400 method
    Return “Ture” if the input number divided by 400, 100 and 4 is 0

--- leapYear.py
+++ leapYear.py
@@ -2,6 +2,9 @@ def is_leapYear(years):
 
     if(years % 4 == 0):
         if (years % 100 == 0):
+            if(years % 400 == 0):
+                return True
+                
             return False
         
         return True
