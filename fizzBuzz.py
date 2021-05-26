75f27261cfecb8f1a7fed546a16f4a7acd51b9c1
Nuocheng Shen <shennu@oregonstate.edu>
2021-05-25 20:37:01 -0700

    Implemented return number method
    Return the input number when the number is neither a multiple of 3 nor a multiple of 5

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,9 +1,11 @@
 def fizzBuzz( input_number ):
     output = ""
-    
+
     if (input_number % 3 == 0):
         output += "Fizz"
     if (input_number % 5 == 0):
         output += "Buzz"
-    
+    if not output:
+        output = input_number
+
     return output
